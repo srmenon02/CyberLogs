d22060925fc210158753c208cad029562e949240
srmenon02 <smenon02@yahoo.com>
2025-07-06 18:05:20 -0700

    MongoDB successfully logging producer logs

--- scripts/test_consumer.py
+++ scripts/test_consumer.py
@@ -1,22 +1,37 @@
 from kafka import KafkaConsumer
+from pymongo import MongoClient
 import json
 import time
 
+# MongoDB Setup
+mongo_client = MongoClient("mongodb://localhost:27017/")
+db = mongo_client["cryptosecure_logs"]  
+collection = db["logs"]                 
+
+#Kafka Consumer Setup
 consumer = KafkaConsumer(
     'test-logs',
     bootstrap_servers='localhost:50849',
     auto_offset_reset='earliest',
-    group_id='my-test-group'
+    enable_auto_commit=True,
+    group_id='my-test-group',
+    value_deserializer=lambda x: json.loads(x.decode('utf-8'))
 )
 
-
-
-print("✅ Connected to Kafka. Listening for messages...\n")
+print("Listening for messages on Kafka and MongoDB\n")
 
 try:
     for message in consumer:
-        print(f"📥 Received message: {message.value}")
+        log_data = message.value
+        log_data['received_at'] = time.strftime('%Y-%m-%d %H:%M:%S')
+
+        collection.insert_one(log_data)
+
+        print(f"Stored in MongoDB: {log_data}")
+
 except KeyboardInterrupt:
-    print("\n🛑 Stopped manually.")
+    print("Keyboard Interrupt.")
+
 finally:
     consumer.close()
+    mongo_client.close()
