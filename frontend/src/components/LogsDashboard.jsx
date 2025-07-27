217c63d65e873f423aa199581f8c456a1bf77e93
srmenon02 <smenon02@yahoo.com>
2025-07-27 13:00:19 -0700

    Updated level dropdown to not be all caps

--- frontend/src/components/LogsDashboard.jsx
+++ frontend/src/components/LogsDashboard.jsx
@@ -121,9 +121,9 @@ export default function LogsDashboard() {
               className="bg-charcoal-700 text-gray-200 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-coral-400"
             >
               <option>All</option>
-              <option>INFO</option>
-              <option>WARNING</option>
-              <option>ERROR</option>
+              <option>Info</option>
+              <option>Warning</option>
+              <option>Error</option>
             </select>
             
             {loading && (
