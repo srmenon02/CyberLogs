8f48e3c3c430bc2127b8388382faf2c283926edf
srmenon02 <smenon02@yahoo.com>
2025-08-01 10:31:37 -0700

    Updated .env

--- backend/kafka_consumer_mongo.py
+++ backend/kafka_consumer_mongo.py
@@ -5,6 +5,9 @@ from kafka import KafkaConsumer
 from motor.motor_asyncio import AsyncIOMotorClient
 import os
 from fastapi import FastAPI
+from dotenv import load_dotenv
+load_dotenv()
+
 
 MONGO_URI = os.getenv("MONGO_URI")
 if not MONGO_URI:
